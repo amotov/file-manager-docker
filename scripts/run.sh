3cb20b9a5227ca25dbcb0c09e36aff0f5b0b1836
amotov <alexandr.motov@kaspibank.kz>
2018-02-07 01:43:01 +0600

    Changed run.sh.

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,5 +2,6 @@ docker run \
   -p 80:8080 \
   -v /home/amotov/Temp:/usr/share/fm/files/writable \
   -v /home/amotov/Work:/usr/share/fm/files/readonly \
+  -e FM_HEAP_SIZE=1024m \
   --name file-manager \
   -d kcell/file-manager:latest
